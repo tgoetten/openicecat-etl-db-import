187a940c86c5510280901421367fc84107d89e4b
Thomas Goetten <thomas.goetten@gmail.com>
2020-05-27 11:10:30 +0200

    - Updated all steps to insert data to use the 'PostgreSQL bulk load' step for better performance \n - The commandline script 'ImportAllsh' is now parameterised - you can choose if you wish to delete all *.xml and/or *.cube files

--- etl/ImportAll.sh
+++ etl/ImportAll.sh
@@ -1,10 +1,89 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# create a backup of serialised-data
-#tar -zcvpf serialised-data/$(date +%Y-%m-%d)-serialised-data-backup.tar.gz serialised-data/*.cube
+# A best practices Bash script template with many useful functions. This file
+# sources in the bulk of the functions from the source.sh file which it expects
+# to be in the same directory. Only those functions which are likely to need
+# modification are present in this file. This is a great combination if you're
+# writing several scripts! By pulling in the common functions you'll minimise
+# code duplication, as well as ease any potential updates to shared functions.
 
-# Delete cube and xml files
-find . -type f -name "*.cube" -exec rm {} \; 
-find . -type f -name "*.xml" -exec rm {} \; 
+# Enable xtrace if the DEBUG environment variable is set
+if [[ ${DEBUG-} =~ ^1|yes|true$ ]]; then
+    set -o xtrace       # Trace the execution of the script (debug)
+fi
 
-sh /opt/data-integration/kitchen.sh -file=ImportAll.kjb ImportAll.kjb -level=Debug -logfile=ImportAll.txt -level=Basic
+# A better class of script...
+set -o errexit          # Exit on most errors (see the manual)
+set -o errtrace         # Make sure any error trap is inherited
+set -o nounset          # Disallow expansion of unset variables
+set -o pipefail         # Use last non-zero exit code in a pipeline
+
+# DESC: Usage help
+# ARGS: None
+# OUTS: None
+function script_usage() {
+    cat << EOF
+Usage:
+     -h|--help                  Displays this help
+     -v|--verbose               Displays verbose output
+    -nc|--no-colour             Disables colour output
+    -dx|--delete-xml		Delete *.xml
+    -dc|--delete-cube		Delete *.cube
+EOF
+}
+
+# DESC: Parameter parser
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: Variables indicating command-line parameters and options
+function parse_params() {
+    local param
+    while [[ $# -gt 0 ]]; do
+        param="$1"
+        shift
+        case $param in
+            -h | --help)
+                script_usage
+                exit 0
+                ;;
+            -v | --verbose)
+                verbose=true
+                ;;
+            -nc | --no-colour)
+                no_colour=true
+                ;;
+            -dx | --delete-xml)
+		deletexml=true
+		;;
+            -dc | --delete-cube)
+                deletecube=true
+                ;;
+            *)
+                script_exit "Invalid parameter was provided: $param" 1
+                ;;
+        esac
+    done
+}
+
+# DESC: Main control flow
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: None
+function main() {
+    # shellcheck source=source.sh
+    source "$(dirname "${BASH_SOURCE[0]}")/source.sh"
+
+    trap script_trap_err ERR
+    trap script_trap_exit EXIT
+
+    script_init "$@"
+    parse_params "$@"
+    colour_init
+    delete_xml
+    delete_cube
+    perform_import
+    #lock_init system
+}
+
+# Make it rain
+main "$@"
+
+# vim: syntax=sh cc=80 tw=79 ts=4 sw=4 sts=4 et sr
