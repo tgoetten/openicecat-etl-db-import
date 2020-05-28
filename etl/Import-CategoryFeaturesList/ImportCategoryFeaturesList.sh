352669062616f4c5cdbe72593d8d3a76441e5dc1
Thomas Goetten <thomas.goetten@gmail.com>
2020-05-28 15:44:48 +0200

    - Removed option to backup .cube files from .sh files

--- etl/Import-CategoryFeaturesList/ImportCategoryFeaturesList.sh
+++ etl/Import-CategoryFeaturesList/ImportCategoryFeaturesList.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# create a backup of serialised-data
-tar -zcvpf serialised-data/$(date +%Y-%m-%d)-serialised-data-backup.tar.gz serialised-data/*.cube
-
-# Delete backup files than 30 days #
-find serialised-data/*.tar.gz -mtime +30 -exec rm {} \;
-
-#cd /opt/data-integration >/dev/null
 sh /opt/data-integration/kitchen.sh -file=ImportCategoryFeaturesList.kjb ImportCategoryFeaturesList.kjb -level=Debug -logfile=ImportCategoryFeaturesList.txt -level=Basic
--- etl/Import-FeatureList/ImportFeaturesList.sh
+++ etl/Import-FeatureList/ImportFeaturesList.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-# create a backup of serialised-data
-tar -zcvpf serialised-data/$(date +%Y-%m-%d)-serialised-data-backup.tar.gz serialised-data/*.cube
-
-# Delete backup files than 30 days #
-find serialised-data/*.tar.gz -mtime +30 -exec rm {} \;
-
-#cd /opt/data-integration >/dev/null
 sh /opt/data-integration/kitchen.sh -file=ImportFeaturesList.kjb ImportFeaturesList.kjb -level=Debug -logfile=ImportFeaturesList.txt -level=Basic
