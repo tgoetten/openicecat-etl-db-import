0584c2ceb03ed5c1336b343f7335136dcc8c6f2f
Thomas Goetten <thomas.goetten@gmail.com>
2020-11-27 09:18:55 +0100

    [ADDED] Added command line parameter to skip download of XML files for Import-ProductDetails if they are already available

--- etl/Import-ProductDetails/create.sql
+++ etl/Import-ProductDetails/create.sql
@@ -16,27 +16,27 @@ CREATE TABLE product__productdetails
   releasedate text,
   thumbpic text,
   thumbpicsize integer,
-  title text,  
-  constraint product_langid FOREIGN KEY (langid) references language (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION, 
+  title text,
+  constraint product_langid FOREIGN KEY (langid) references language (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
   CONSTRAINT product__productdetails_prod_id_ukey unique (product_id, langid)
   ) WITH (OIDS = FALSE);
 
 CREATE TABLE product__summarydescription (
-  ID SERIAL PRIMARY KEY, 
+  ID SERIAL PRIMARY KEY,
   shortsummarydescription_langid integer,
-  shortsummarydescription_shortsummarydescription text,	
+  shortsummarydescription_shortsummarydescription text,
   longsummarydescription_langid integer,
   longsummarydescription_longsummarydescription text,
   product_id integer,
---  constraint product__summarydescription_product FOREIGN KEY (product_id) references product (product_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION, 
-  constraint product__summarydescription_shortsummarydescription_langid FOREIGN KEY (shortsummarydescription_langid) references language (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION, 
+--  constraint product__summarydescription_product FOREIGN KEY (product_id) references product (product_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
+  constraint product__summarydescription_shortsummarydescription_langid FOREIGN KEY (shortsummarydescription_langid) references language (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
   constraint product__summarydescription_longsummarydescription_langid FOREIGN KEY (longsummarydescription_langid) references language (id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 ) WITH (OIDS = FALSE);
 
 
 CREATE TABLE product__productfeature (
   ID SERIAL PRIMARY KEY,
-  productfeature_id integer, 
+  productfeature_id integer,
   local_id integer,
   value text,
   localized text,
@@ -64,11 +64,11 @@ CREATE TABLE product__productfeature (
   categoryfeaturegroup_id integer,
 --  constraint product__productfeature_product FOREIGN KEY (product_id) references product (product_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 ) WITH (OIDS = FALSE);
-    
+
 CREATE TABLE product__productrelated (
-  ID SERIAL PRIMARY KEY, 
+  ID SERIAL PRIMARY KEY,
   __product_id integer, -- taken from individual xml file to referr to the product that those relations apply to!
-  productrelated_id integer,  
+  productrelated_id integer,
   product_id integer,
   category_id integer,
   preferred integer,
@@ -77,7 +77,7 @@ CREATE TABLE product__productrelated (
   product_name text,
   product__supplier_id integer,
   product__supplier_name text,
---  constraint product__productrelated_product FOREIGN KEY (__product_id) references product (product_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION, 
+--  constraint product__productrelated_product FOREIGN KEY (__product_id) references product (product_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
   constraint product__productrelated_supplier FOREIGN KEY (product__supplier_id) references supplier (ID) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 ) WITH (OIDS = FALSE);
 
@@ -95,7 +95,33 @@ CREATE TABLE product__productdescription (
   url text,
   warrantyinfo text,
   langid integer,
---  constraint product__productdescription_product FOREIGN KEY (product_id) references product (product_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION, 
+--  constraint product__productdescription_product FOREIGN KEY (product_id) references product (product_id) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION,
   constraint product__productdescription_langid FOREIGN KEY (langid) references language (ID) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION
 ) WITH (OIDS = FALSE);
 
+CREATE TABLE product__productpicture (
+  product_id integer,
+  ID SERIAL PRIMARY KEY,
+  productpicture_ismain text,
+  productpicture_lowpic text,
+  productpicture_lowpicheight integer,
+  productpicture_lowpicwidth integer,
+  productpicture_lowsize integer,
+  productpicture_no integer,
+  productpicture_original text,
+  productpicture_originalsize integer,
+  productpicture_pic text,
+  productpicture_pic500x500 text,
+  productpicture_pic500x500height integer,
+  productpicture_pic500x500size integer,
+  productpicture_pic500x500width integer,
+  productpicture_picheight integer,
+  productpicture_picwidth integer,
+  productpicture_productpicture_id integer,
+  productpicture_size integer,
+  productpicture_source text,
+  productpicture_thumbpic text,
+  productpicture_thumbsize integer,
+  productpicture_type text,
+  productpicture_updateddate text
+) WITH (OIDS = FALSE);
