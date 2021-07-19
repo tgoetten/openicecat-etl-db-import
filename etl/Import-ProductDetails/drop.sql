f4fc3b67bc5cca0d2a1f7b992b16211715ec2864
Thomas Goetten <thomas.goetten@gmail.com>
2021-07-19 07:12:21 +0200

    [UPDATE] Step to Import-ProductDetails and added the langid to ProductRelated; SQLs to CREATE and DROP database where changed accordingly; also removed pghero from docker-compose

--- etl/Import-ProductDetails/drop.sql
+++ etl/Import-ProductDetails/drop.sql
@@ -4,3 +4,4 @@ DROP TABLE IF EXISTS product__productfeature CASCADE;
 DROP TABLE IF EXISTS product__productrelated CASCADE;
 DROP TABLE IF EXISTS product__productdescription CASCADE;
 DROP TABLE IF EXISTS product__productpicture CASCADE;
+DROP TABLE IF EXISTS product__productcategoryfeaturegroup CASCADE;
