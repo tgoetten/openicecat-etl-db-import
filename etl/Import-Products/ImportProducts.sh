f8be364ce095b39a39555f0aa2bfbbaba0f04865
Thomas Goetten <thomas.goetten@gmail.com>
2021-07-19 15:29:53 +0200

    [UPDATE] .gitignore & .SH script to import Products

--- etl/Import-Products/ImportProducts.sh
+++ etl/Import-Products/ImportProducts.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export PENTAHO_DI_JAVA_OPTIONS="-Xmx16g -XX:MaxPermSize=512m" && sh /opt/data-integration/kitchen.sh -file=ImportProducts.kjb ImportProducts.kjb -level=Basic -logfile=ImportProducts.txt -param:extract=false -param:filename=files.index.xml -param:langid=1 -param:url=http://data.icecat.biz/export/freexml.int/EN/files.index.xml -param:drop=true -param:create=true -param:cube=true
 #export PENTAHO_DI_JAVA_OPTIONS="-Xmx16g -XX:MaxPermSize=512m" && sh /opt/data-integration/kitchen.sh -file=ImportProducts.kjb ImportProducts.kjb -level=Basic -logfile=ImportProducts.txt -param:extract=false -param:filename=files.index.xml -param:langid=1 -param:url=http://data.icecat.biz/export/freexml.int/EN/files.index.xml -param:drop=true -param:create=true -param:cube=true
+export PENTAHO_DI_JAVA_OPTIONS="-Xmx16g -XX:MaxPermSize=512m" && sh /opt/data-integration/kitchen.sh -file=ImportProducts.kjb ImportProducts.kjb -level=Basic -logfile=ImportProducts.txt -param:extract=false -param:filename=files.index.xml -param:langid=1 -param:url=http://data.icecat.biz/export/freexml.int/EN/files.index.xml -param:drop=true -param:create=true -param:cube=false
